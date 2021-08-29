7f1683ee7d65e6059d3fb412eb7092f784292655
KengoWakida <wakida.kengo.mac@gmail.com>
2021-08-29 20:54:36 +0900

    バリデーションの設定（正しく入力した時）

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
   #Basic認証
   before_action :basic_auth
-
+  
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def show 
+    @user = User.find(params[:id])
+  end
 
   private
   #Basic認証
