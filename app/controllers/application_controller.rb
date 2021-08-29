bd2b297867ecab841cb2b2bee24f527a3fc23d2f
KengoWakida <wakida.kengo.mac@gmail.com>
2021-08-29 19:33:14 +0900

    バリデーションの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,18 @@ class ApplicationController < ActionController::Base
   #Basic認証
   before_action :basic_auth
 
-  private
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
+  private
+  #Basic認証
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == 'admin' && password == '2222'
     end
+  end
+
+  #新規登録画面でのバリデーション
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,keys: [:nickname,:lastname,:firstname,:lastname_kana,:firstname_kana,:birthday])
   end
 end
