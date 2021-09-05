5735991fd487bddbeb7f9dfd25725b2a66879b60
KengoWakida <wakida.kengo.mac@gmail.com>
2021-09-05 14:47:02 +0900

    FactoryBotの設定

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname {'furima太郎'}
+    email {'test@furima'}
+    password {'test12'}
+    lastname {'婦利増'}
+    firstname {'太郎'}
+    lastname_kana {'フリマ'}
+    firstname_kana {'タロウ'}
+    birthday {'2000/6/8'}
   end
 end
