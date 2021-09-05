050b5f7823abc252dd1d0b73b3b02dfcea48f490
KengoWakida <wakida.kengo.mac@gmail.com>
2021-09-05 13:55:12 +0900

    バリデーションの設定（入力値の制限）

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,21 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+
+        #パスワードが半角英数字で混合しなければいけない
+        VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+        validates :encrypted_password, format: { with: VALID_PASSWORD_REGEX, message: "is invalid.Include both letters and numbers"}
          #バリデーション カラムが空では保存できない
-         validates :nickname,           presence: true
-         validates :lastname,           presence: true
-         validates :firstname,          presence: true
-         validates :lastname_kana,      presence: true
-         validates :firstname_kana,     presence: true
-         validates :birthday,           presence: true
+      with_options presence: true do
+         validates :nickname   
+        #漢字、ひらがな、カタカナのみ許可
+         validates :lastname,format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
+         validates :firstname,format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
+         #カタカナのみ許可
+         validates :lastname_kana, format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
+         validates :firstname_kana,  format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
+         validates :birthday
+      end
+
+      
 end
