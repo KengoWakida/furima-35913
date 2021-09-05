03f02169ea5b3604cc350ddb2236c705dcc1ccf7
KengoWakida <wakida.kengo.mac@gmail.com>
2021-09-05 17:27:21 +0900

    Userモデルのテスト（内容に問題がある場合）

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,61 +14,101 @@ RSpec.describe User, type: :model do
 
     context '内容に問題がある場合' do
       it 'nicknameが空だと保存できないこと' do
-
+        @user.nickname = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
       end
       it 'emailが空だと保存できないこと' do
-
+        @user.email = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email can't be blank")
       end
       it 'emailが重複していると保存できないこと' do
-
+        @user.save
+        another_user = FactoryBot.build(:user)
+        another_user.email = @user.email
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include('Email has already been taken')
       end
       it 'emailに@が含まれていないと保存できないこと' do
-
+        @user.email = 'testfurima'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email is invalid")
       end
-      it 'encrypted_passwordが空だと保存できないこと' do
+      it 'passwordが空だと保存できないこと' do
+        @user.password = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank")
         
       end
-      it 'encrypted_passwordが5文字以下だと保存できないこと' do
-      
+      it 'passwordが5文字以下だと保存できないこと' do
+        @user.password = 'test1'
+        @user.password_confirmation = @user.password
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
       end
-      it 'encrypted_passwordが英字だけだと保存できないこと' do
-
+      it 'passwordが英字だけだと保存できないこと' do
+        @user.password = 'abcdef'
+        @user.password_confirmation = @user.password
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is invalid.Include both letters and numbers")
       end
       it 'encrypted_passwordが数字だけだと保存できないこと' do
-
+        @user.password = '123456'
+        @user.password_confirmation = @user.password
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is invalid.Include both letters and numbers")
       end
-      it 'encrypted_passwordとpassword_confirmationが一致しなければ保存できないこと' do
-
+      it 'passwordとpassword_confirmationが一致しなければ保存できないこと' do
+        @user.password = 'test00'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
       it 'lastnameが空だと保存できないこと' do
-      
+        @user.lastname = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Lastname can't be blank")
       end
       it 'lastnameが全角日本語でないと保存できないこと' do
-      
+        @user.lastname = 'furima'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Lastname is invalid. Input full-width characters.")
       end
       it 'firstnameが空だと保存できないこと' do
-      
+        @user.firstname = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Firstname can't be blank")
       end
       it 'firstnameが全角日本語でないと保存できないこと' do
-      
+        @user.firstname = 'tarou'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Firstname is invalid. Input full-width characters.")
       end
       it 'lastname_kanaが空だと保存できないこと' do
-      
+        @user.lastname_kana = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Lastname kana can't be blank")
       end
       it 'lastname_kanaが全角カタカナでないと保存できないこと' do
-      
+        @user.lastname_kana = 'ふりま'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Lastname kana is invalid. Input full-width katakana characters.")
       end
       it 'firstname_kanaが空だと保存できないこと' do
-      
+        @user.firstname_kana = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Firstname kana can't be blank")
       end
       it 'firstname_kanaが全角カタカナでないと保存できないこと' do
-      
+        @user.firstname_kana = 'たろう'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Firstname kana is invalid. Input full-width katakana characters.")
       end
       it 'birthdayが空だと保存できないこと' do
-      
+        @user.birthday = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Birthday can't be blank")
       end
-
     end
-  
   end
 end
