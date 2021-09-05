c45464fba2d688bd0642c8123fd5ebe97d4022d1
KengoWakida <wakida.kengo.mac@gmail.com>
2021-09-05 19:08:49 +0900

    レビュー後の修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,6 @@ class ApplicationController < ActionController::Base
   
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def show 
-    @user = User.find(params[:id])
-  end
-
   private
   #Basic認証
   def basic_auth
