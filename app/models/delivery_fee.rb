9e457d2f22d5381fa6c3d1007d59d904b84a1dd6
KengoWakida <wakida.kengo.mac@gmail.com>
2021-09-08 00:26:38 +0900

    Active Hashのテーブル作成

---
+++ app/models/delivery_fee.rb
@@ -0,0 +1,8 @@
+class Delivery_fee < ActiveHash::Base
+  self.data = [
+    {id: 0, name: '--'}, {id: 1, name: '着払い'}, {id: 2, name: '送料込み'}
+              ]
+
+  include ActiveHash::Associations
+  has_many :items
+end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,10 @@
 class Item < ApplicationRecord
   belongs_to :users
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :delivery_fee
+  belongs_to :prefecture
+  belongs_to :days
 end
