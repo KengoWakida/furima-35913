10d7fd71e5a8ef6794fb3e8ac448d0aa52a30092
KengoWakida <wakida.kengo.mac@gmail.com>
2021-09-08 23:08:21 +0900

    アクティブハッシュのバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,15 @@ class Item < ApplicationRecord
   belongs_to :delivery_fee
   belongs_to :prefecture
   belongs_to :days
+
+  #カテゴリーの選択が「--」の時は保存できないようにする
+  validates :category_id, numericality: { other_than: 0 , message: "can't be blank"}
+  #商品の状態の選択が「--」の時は保存できないようにする
+  validates :status_id, numericality: { other_than: 0 , message: "can't be blank"}
+  #配達料の選択が「--」の時は保存できないようにする
+  validates :delivery_fee_id, numericality: { other_than: 0 , message: "can't be blank"}
+  #都道府県の選択が「--」の時は保存できないようにする
+  validates :prefecture_id, numericality: { other_than: 0 , message: "can't be blank"}
+  #日数の選択が「--」の時は保存できないようにする
+  validates :days_id, numericality: { other_than: 0 , message: "can't be blank"}
 end
