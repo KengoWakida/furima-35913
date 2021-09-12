68f7aafca97c63ee12ee32bbb1d0c43d0dc5b391
KengoWakida <wakida.kengo.mac@gmail.com>
2021-09-12 14:50:54 +0900

    バリデーション、エラーメッセージの表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,16 +8,24 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :days
 
-  #カテゴリーの選択が「--」の時は保存できないようにする
-  validates :category_id, numericality: { other_than: 0 , message: "can't be blank"}
-  #商品の状態の選択が「--」の時は保存できないようにする
-  validates :status_id, numericality: { other_than: 0 , message: "can't be blank"}
-  #配達料の選択が「--」の時は保存できないようにする
-  validates :delivery_fee_id, numericality: { other_than: 0 , message: "can't be blank"}
-  #都道府県の選択が「--」の時は保存できないようにする
-  validates :prefecture_id, numericality: { other_than: 0 , message: "can't be blank"}
-  #日数の選択が「--」の時は保存できないようにする
-  validates :days_id, numericality: { other_than: 0 , message: "can't be blank"}
-  #価格は¥300〜¥9,999,999まで半角数値のみ保存
-  validates :price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is invalid"}
-end
+    #バリデーション カラムが空では保存できない
+    with_options presence: true do
+      validates :image
+      validates :product_name
+      validates :explanation
+      #カテゴリーの選択が「--」の時は保存できないようにする
+      validates :category_id, numericality: { other_than: 0 , message: "can't be blank"}
+      #商品の状態の選択が「--」の時は保存できないようにする
+      validates :status_id, numericality: { other_than: 0 , message: "can't be blank"}
+      #配達料の選択が「--」の時は保存できないようにする
+      validates :delivery_fee_id, numericality: { other_than: 0 , message: "can't be blank"}
+      #都道府県の選択が「--」の時は保存できないようにする
+      validates :prefecture_id, numericality: { other_than: 0 , message: "can't be blank"}
+      #日数の選択が「--」の時は保存できないようにする
+      validates :days_id, numericality: { other_than: 0 , message: "can't be blank"}
+      #価格は¥300〜¥9,999,999まで半角数値のみ保存
+      validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is invalid"}
+    end
+
+ 
+  end
