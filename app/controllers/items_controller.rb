4e1c3b2d0bffc6714061023eae06632886a6656d
KengoWakida <wakida.kengo.mac@gmail.com>
2021-09-15 00:11:15 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,6 @@ class ItemsController < ApplicationController
 
   def index
     @items = Item.order("created_at DESC")
-
   end
 
   def new
@@ -13,7 +12,6 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.create(item_params)
-    binding.pry
     if @item.save
       redirect_to root_path
     else
