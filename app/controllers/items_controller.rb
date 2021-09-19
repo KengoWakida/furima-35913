7b5ff05457bcc2d321427f9af6874da8dc8cf0f6
KengoWakida <wakida.kengo.mac@gmail.com>
2021-09-19 15:18:11 +0900

    ItemコントローラーUpdateアクションの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -28,6 +28,17 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    binding.pry
+    if item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   private
     def item_params
       params.require(:item).permit(:image,:product_name,:explanation,:category_id,:status_id,:delivery_fee_id,:prefecture_id,:days_id,:price).merge(user_id: current_user.id)
