ec844ee0c24207b3572a25fc79adf6f50d61417c
KengoWakida <wakida.kengo.mac@gmail.com>
2021-09-20 19:08:50 +0900

    商品購入画面への値の遷移

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,11 @@
 class OrdersController < ApplicationController
   def index
     #フォームオブジェクトのインスタンス生成
+    #binding.pry
+    @item = Item.find(params[:item_id])
+  end
+
+  def create
+    #binding.pry
   end
 end
