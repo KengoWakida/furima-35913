7fadcdc0b51ae3438f19d133f89693f8352dffbf
KengoWakida <wakida.kengo.mac@gmail.com>
2021-09-20 16:21:18 +0900

    Formオブジェクトorder_shippingの実装

--- app/models/order_shipping.rb
+++ app/models/order_shipping.rb
@@ -1,3 +1,22 @@
 class OrderShipping
+  include ActiveModel::Model
+  attr_accessor :token, :user_id, :item_id, :postal_code, :prefecture_id, :unicipality, :address, :building, :tel_number, :order_id
+  
+  with_options presence: true do
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is _invalid. Include hyphen(-)"}
+    #都道府県の選択が「--」の時は保存できないようにする
+    validates :prefecture_id, numericality: { other_than: 0 , message: "can't be blank"}
+    validates :unicipality
+    validates :address
+    validates :tel_number {only_integer: true, message: 'is invalid'}
+    validates :order_id
+  end
+
+def save
+  #購入記録を保存する
+  Order.create(user_id: user_id, item_id: item_id)
+  #発送先情報を保存する
+  Shipping.create(postal_code: postal_code, prefecture_id: prefecture_id, unicipality: unicipality, address: address, building: building, tel_number: tel_number, order_id: order.id)
+end
 
 end
