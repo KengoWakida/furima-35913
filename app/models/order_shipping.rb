0a2375962c7b3ae5917c0c45d792ce32d85a551c
KengoWakida <wakida.kengo.mac@gmail.com>
2021-09-20 16:32:10 +0900

    Update order_shipping.rb

--- app/models/order_shipping.rb
+++ app/models/order_shipping.rb
@@ -8,7 +8,7 @@ class OrderShipping
     validates :prefecture_id, numericality: { other_than: 0 , message: "can't be blank"}
     validates :unicipality
     validates :address
-    validates :tel_number {only_integer: true, message: 'is invalid'}
+    validates :tel_number {with: /\A[0-9]{10..11}\z/, message: 'is invalid'}
     validates :order_id
   end
 
