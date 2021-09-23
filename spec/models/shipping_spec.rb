640a7adb464f7415bd8c1ab56702c68d088e5eb5
KengoWakida <wakida.kengo.mac@gmail.com>
2021-09-23 19:08:27 +0900

    RSpecのshippingテーブルの単体テストの雛形

--- spec/models/shipping_spec.rb
+++ spec/models/shipping_spec.rb
@@ -1,5 +1,74 @@
 require 'rails_helper'
 
 RSpec.describe Shipping, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '配送先情報の保存' do
+    context '配送先情報が保存できる場合' do
+      it 'すべての項目
+        post_code(郵便番号)、
+        prefecture_id(都道府県)、
+        unicipality(市区町村)、
+        address(番地)、
+        building(建物)、
+        tel_number(電話番号)（10桁の入力）
+        が正しく入力されている場合、保存できる' do
+      end
+      it 'すべての項目
+        post_code(郵便番号)、
+        prefecture_id(都道府県)、
+        unicipality(市区町村)、
+        address(番地)、
+        building(建物)、
+        tel_number(電話番号)（11桁の入力）
+        が正しく入力されている場合、保存できる' do
+      end
+      it 'building(建物名)が空欄の場合、保存できる' do
+        
+      end
+    end
+    context '配送先情報が保存されない場合' do
+      it 'post_code(郵便番号)が空欄の場合、保存できない' do
+        
+      end
+      it 'post_code(郵便番号)にハイフン（-）がない場合、保存できない' do
+        
+      end
+      it 'post_code(郵便番号)が英字の場合、保存できない' do
+        
+      end
+      it 'post_code(郵便番号)が全角の場合、保存できない' do
+        
+      end
+      it 'prefecture_id(都道府県)が選択されていない場合、保存できない' do
+        
+      end
+      it 'unicipality(市区町村)が選択されていない場合、保存できない' do
+        
+      end
+      it 'address(番地)が選択されていない場合、保存できない' do
+        
+      end
+      it 'tel_number(電話番号)が選択されていない場合、保存できない' do
+        
+      end
+      it 'tel_number(電話番号)が9桁以下の場合、保存できない' do
+        
+      end
+      it 'tel_number(電話番号)が12桁以上の場合、保存できない' do
+        
+      end
+      it 'tel_number(電話番号)が全角の場合、保存できない' do
+        
+      end
+      it 'tel_number(電話番号)が英字の場合、保存できない' do
+        
+      end
+      it 'item(商品情報)が紐づいていない場合、保存できない' do
+        
+      end
+      it 'user(ユーザー情報)が紐づいていない場合、保存できない' do
+        
+      end
+    end
+  end
+
 end
