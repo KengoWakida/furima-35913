a4ca06fc055f930daefcdce67be46e9f4417e7fe
KengoWakida <wakida.kengo.mac@gmail.com>
2021-09-26 17:41:54 +0900

    カード情報の削除

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -7,6 +7,7 @@ const pay = () => {
     const formResult = document.getElementById("charge-form");
     const formData = new FormData(formResult);
 
+    //カード情報の取得
     const card = {
       number: formData.get("order_shipping[number]"),
       cvc: formData.get("order_shipping[cvc]"),
@@ -14,11 +15,22 @@ const pay = () => {
       exp_year: `20${formData.get("order_shipping[exp_year]")}`,
     };
 
+    //トークンの作成
     Payjp.createToken(card, (status, response) => {
       if (status == 200) {
         const token = response.id;
-        console.log(token)
+        const renderDom = document.getElementById("charge-form");
+        const tokenObj = `<input value=${token} name='token' type="hidden">`;
+        renderDom.insertAdjacentHTML("beforeend",tokenObj);
       }
+     
+      //カード情報の削除
+      document.getElementById("card-number").removeAttribute("name");
+      document.getElementById("card-cvc").removeAttribute("name");
+      document.getElementById("card-exp-month").removeAttribute("name");
+      document.getElementById("card-exp-year").removeAttribute("name");
+
+      document.getElementById("charge-form").submit();
     });
   });
 };
