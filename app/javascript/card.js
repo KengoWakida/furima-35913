94822eaeb7881c098cdd2de735e7d5215e8ad7a5
KengoWakida <wakida.kengo.mac@gmail.com>
2021-09-26 16:13:29 +0900

    カード情報のトークン化

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,9 +1,25 @@
-const pay = () =>{
+const pay = () => {
+  Payjp.setPublicKey("pk_test_b0c942412ebcb27275d749e4");  //PAY.JPテスト公開鍵
   const submit = document.getElementById("button");
   submit.addEventListener("click",(e) => {
     e.preventDefault();
 
-    console.log("フォーム送信時にイベント発火");
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("order_shipping[number]"),
+      cvc: formData.get("order_shipping[cvc]"),
+      exp_month: formData.get("order_shipping[exp_month]"),
+      exp_year: `20${formData.get("order_shipping[exp_year]")}`,
+    };
+
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200) {
+        const token = response.id;
+        console.log(token)
+      }
+    });
   });
 };
 
