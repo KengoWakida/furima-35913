75540161482bf0d4301545f7e18dcc6c9adfac7e
KengoWakida <wakida.kengo.mac@gmail.com>
2021-10-02 17:10:53 +0900

    テーブル修正

--- db/schema.rb
+++ db/schema.rb
@@ -33,6 +33,11 @@ ActiveRecord::Schema.define(version: 2021_09_20_074041) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "hoges", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "product_name", null: false
     t.text "explanation", null: false
